50b2e379e4f9464effc923f90dca7ad826d97df0
abbabamba <babacarbamba11@gmail.com>
2024-08-05 22:04:56 +0000

    face component created/added in parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
+  imports:[
+    FaceSnapComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tpAngular1';
+  //title = 'tpAngular1';
 }
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+  title!:string;
+  description!:string;
+  createdAt!:Date;
+  snaps!:number;
+
+  ngOnInit(): void {
+    this.title="My title facesnap";
+    this.description="My description facesnap";
+    this.createdAt=new Date();
+    this.snaps=10;
+  }
+
+}
