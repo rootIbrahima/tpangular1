b9c523797b319cc29918d9050a11e032e7f5def6
Ibrahima <lyibrahima321@gmail.com>
2024-08-06 14:55:20 +0000

    add image

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,12 +12,14 @@ export class FaceSnapComponent implements OnInit {
   description!:string;
   createdAt!:Date;
   snaps!:number;
+  imageUrl:string |undefined;
 
   ngOnInit(): void {
     this.title="My title facesnap";
     this.description="My description facesnap";
     this.createdAt=new Date();
     this.snaps=10;
+    this.imageUrl = 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg';
   }
 
 }
