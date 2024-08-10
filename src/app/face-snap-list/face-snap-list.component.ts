6c1a1249fb4004097ceae2b8ce6fa0bbf7645299
Ibrahima <lyibrahima321@gmail.com>
2024-08-10 17:50:43 +0000

    Add services and injection on face-list

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
+import { FaceSnapService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -11,32 +12,10 @@ import { FaceSnapComponent } from '../face-snap/face-snap.component';
 })
 export class FaceSnapListComponent implements OnInit{
   myFaceSnap!:FaceSnap[];
+  constructor(private faceSnapService:FaceSnapService){}
   ngOnInit(): void {
-    this.myFaceSnap=[
-      new FaceSnap(
-        "My first website",
-    "Ceci est une plateforme de publication des photos",
-    'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
-    new Date(),
-    0,
-      )
-      ,
-      new FaceSnap(
-        "Mon deuxieme site",
-      "Ceci est une plateforme de publication des videos",
-      'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg',
-      new Date(),
-      0,
-      ),
-      new FaceSnap(
-        "Mon troisieme site",
-        "Ceci est une plateforme de publication des jeu",
-         'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
-        new Date(),
-        0,
-      ),
+    this.myFaceSnap= this.faceSnapService.getFaceSnaps();
 
-    ];
     this.myFaceSnap[0].setLocation("Dakar,Senegal");
     this.myFaceSnap[1].setLocation("Paris,France");
     this.myFaceSnap[2].setLocation("Washington,US");
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap";
 
 @Injectable({
     providedIn:"root"
 })
 export class FaceSnapService{
-
+    private monFaceSnap:FaceSnap[]=[
+        new FaceSnap(
+          "My first website",
+      "Ceci est une plateforme de publication des photos",
+      'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
+      new Date(),
+      0,
+        )
+        ,
+        new FaceSnap(
+          "Mon deuxieme site",
+        "Ceci est une plateforme de publication des videos",
+        'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg',
+        new Date(),
+        0,
+        ),
+        new FaceSnap(
+          "Mon troisieme site",
+          "Ceci est une plateforme de publication des jeu",
+           'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+          new Date(),
+          0,
+        ),
+  
+      ];
+      getFaceSnaps():FaceSnap[]{
+        return[...this.monFaceSnap]
+      }
 }
