0b4c90d2b5f610200391a0857e8358a2a410bf34
Ibrahima <lyibrahima321@gmail.com>
2024-08-11 19:36:46 +0000

    Add assets/images and css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-    {path:"facesnaps",component:FaceSnapListComponent}
+    {path:"facesnaps",component:FaceSnapListComponent},
+    {path:'',component:LandingPageComponent}
 ];
